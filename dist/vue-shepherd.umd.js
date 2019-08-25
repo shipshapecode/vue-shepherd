b73fd32fb15abd06bd509ea83b25714761586139
Robert Wagner <rwwagner90@gmail.com>
2019-08-25 14:32:13 -0400

    Add dist

---
+++ dist/vue-shepherd.umd.js
@@ -0,0 +1,76 @@
+/*!
+ * vue-shepherd v0.0.0 
+ * (c) 2019 
+ * Released under the undefined License.
+ */
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('shepherd.js')) :
+  typeof define === 'function' && define.amd ? define(['shepherd.js'], factory) :
+  (global = global || self, global.VueShepherd = factory(global.Shepherd));
+}(this, function (Shepherd) { 'use strict';
+
+  Shepherd = Shepherd && Shepherd.hasOwnProperty('default') ? Shepherd['default'] : Shepherd;
+
+  function _setPrototypeOf(o, p) {
+    _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
+      o.__proto__ = p;
+      return o;
+    };
+
+    return _setPrototypeOf(o, p);
+  }
+
+  function isNativeReflectConstruct() {
+    if (typeof Reflect === "undefined" || !Reflect.construct) return false;
+    if (Reflect.construct.sham) return false;
+    if (typeof Proxy === "function") return true;
+
+    try {
+      Date.prototype.toString.call(Reflect.construct(Date, [], function () {}));
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  function _construct(Parent, args, Class) {
+    if (isNativeReflectConstruct()) {
+      _construct = Reflect.construct;
+    } else {
+      _construct = function _construct(Parent, args, Class) {
+        var a = [null];
+        a.push.apply(a, args);
+        var Constructor = Function.bind.apply(Parent, a);
+        var instance = new Constructor();
+        if (Class) _setPrototypeOf(instance, Class.prototype);
+        return instance;
+      };
+    }
+
+    return _construct.apply(null, arguments);
+  }
+
+  var version = '0.0.0';
+
+  var install = function install(Vue) {
+    Vue.prototype.$shepherd = function () {
+      for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+        args[_key] = arguments[_key];
+      }
+
+      return _construct(Shepherd.Tour, args);
+    };
+  };
+
+  var plugin = {
+    install: install,
+    version: version
+  };
+
+  if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(plugin);
+  }
+
+  return plugin;
+
+}));
